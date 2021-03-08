0a2d222532db770dfbe566e658b2a9f5bed156c1
Aleksandar Todorov <atodorov284@gmail.com>
2021-03-08 13:25:32 +0200

    fixing validation rules

---
+++ five_donors.sql
@@ -0,0 +1,8 @@
+USE `donor_system`;
+INSERT INTO donors (name, email, password, phoneNumber, bloodGroup)
+VALUES 
+("Petur Stoqnov", "petur.stoqnov@abv.bg", "btWDPPNShuv4Zit7WUnw10K77D8=", "0881231234", "A+"),
+("Dimitur Petrov", "dimitur.petrov@abv.bg", "btWDPPNShuv4Zit7WUnw10K77D8=", "0881231234", "0+"),
+("Stefan Stoqnov", "stefan.stoqnov@abv.bg", "btWDPPNShuv4Zit7WUnw10K77D8=", "0881231234", "0-"),
+("Teodora Stoqnova", "teodora.stoqnova@abv.bg", "btWDPPNShuv4Zit7WUnw10K77D8=", "0881231234", "B-"),
+("Pesho Petrov", "pesho.petrov@abv.bg", "btWDPPNShuv4Zit7WUnw10K77D8=", "0881231234","0+");
